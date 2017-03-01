2880b0eae682f92e4903a7707ad7b66647bbeb62
jchua1 <jchua@stuy.edu>
2017-03-01 02:12:13 -0500

    almost done

--- svg.js
+++ svg.js
@@ -1,10 +1,39 @@
 var pic = document.getElementById("vimage");
+var b = document.getElementById("button");
 
-var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-c.setAttribute("cx", 0);
-c.setAttribute("cy", 0);
-c.setAttribute("r", "100");
-c.setAttribute("fill", "red");
-c.setAttribute("stroke", "black");
+var first = true;
+var prevX, prevY = 0;
 
-pic.appendChild(c);
+var dots = function(e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    prevX = x;
+    prevY = y;
+    var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
+    if (!first) {
+	var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+	l.setAttribute("x1", prevX);
+	l.setAttribute("y1", prevY);
+	l.setAttribute("x2", x);
+	l.setAttribute("y2", y);
+	l.setAttribute("stroke", "black");
+	pic.appendChild(l);
+    };
+    first = false;
+    c.setAttribute("cx", x);
+    c.setAttribute("cy", y);
+    c.setAttribute("r", 10);
+    c.setAttribute("fill", "red");
+    c.setAttribute("stroke", "black");
+    pic.appendChild(c);
+};
+
+var clear = function() {
+    while (pic.lastChild)
+	pic.removeChild(pic.lastChild);
+};
+
+pic.addEventListener("click", dots);
+b.addEventListener("click", clear);
+
+
