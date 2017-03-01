489b60ea2ca1bd3a0e75e14d014be88ff1c86112
jchua1 <jchua@stuy.edu>
2017-03-01 02:19:29 -0500

    done

--- svg.js
+++ svg.js
@@ -4,22 +4,8 @@ var b = document.getElementById("button");
 var first = true;
 var prevX, prevY = 0;
 
-var dots = function(e) {
-    var x = e.offsetX;
-    var y = e.offsetY;
-    prevX = x;
-    prevY = y;
+var circle = function(x, y) {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
-    if (!first) {
-	var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
-	l.setAttribute("x1", prevX);
-	l.setAttribute("y1", prevY);
-	l.setAttribute("x2", x);
-	l.setAttribute("y2", y);
-	l.setAttribute("stroke", "black");
-	pic.appendChild(l);
-    };
-    first = false;
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
     c.setAttribute("r", 10);
@@ -28,9 +14,31 @@ var dots = function(e) {
     pic.appendChild(c);
 };
 
+var line = function(x, y) {
+    var l = document.createElementNS("http://www.w3.org/2000/svg", "line");
+    l.setAttribute("x1", prevX);
+    l.setAttribute("y1", prevY);
+    l.setAttribute("x2", x);
+    l.setAttribute("y2", y);
+    l.setAttribute("stroke", "black");
+    pic.appendChild(l);
+};
+
+var dots = function(e) {
+    var x = e.offsetX;
+    var y = e.offsetY;
+    circle(x, y);
+    if (!first)
+	line(x, y);
+    first = false;
+    prevX = x;
+    prevY = y;
+};
+
 var clear = function() {
     while (pic.lastChild)
 	pic.removeChild(pic.lastChild);
+    first = true;
 };
 
 pic.addEventListener("click", dots);
