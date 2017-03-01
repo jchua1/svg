2c5ecbd60127605176c4723e6d4e694fb6aaa105
jchua1 <jchua@stuy.edu>
2017-03-01 14:06:18 -0500

    finished growing circle

--- svg.js
+++ svg.js
@@ -4,11 +4,11 @@ var b = document.getElementById("button");
 var first = true;
 var prevX, prevY = 0;
 
-var circle = function(x, y) {
+var circle = function(x, y, r) {
     var c = document.createElementNS("http://www.w3.org/2000/svg", "circle");
     c.setAttribute("cx", x);
     c.setAttribute("cy", y);
-    c.setAttribute("r", 10);
+    c.setAttribute("r", r);
     c.setAttribute("fill", "red");
     c.setAttribute("stroke", "black");
     pic.appendChild(c);
@@ -27,7 +27,7 @@ var line = function(x, y) {
 var dots = function(e) {
     var x = e.offsetX;
     var y = e.offsetY;
-    circle(x, y);
+    circle(x, y, 10);
     if (!first)
 	line(x, y);
     first = false;
@@ -35,13 +35,38 @@ var dots = function(e) {
     prevY = y;
 };
 
-var clear = function() {
-    while (pic.lastChild)
-	pic.removeChild(pic.lastChild);
-    first = true;
+var requestID;
+
+var grow = function() {
+    
+    window.cancelAnimationFrame(requestID);
+
+    var radius = 50;
+    var xcor = 250;
+    var ycor = 250;
+    var increment = 1;
+    
+    var draw = function() {
+	if (pic.lastChild)
+	    pic.removeChild(pic.lastChild);
+	circle(xcor, ycor, radius);
+	radius += increment;
+	if (xcor + radius == 500 || radius == 0)
+	    increment *= -1;
+    requestID = window.requestAnimationFrame(draw);
+    };
+    
+    draw();
 };
 
-pic.addEventListener("click", dots);
+var clear = function() {
+    window.cancelAnimationFrame(requestID);
+    //while (pic.lastChild)
+    pic.removeChild(pic.lastChild);
+    //first = true;
+};
+
+pic.addEventListener("click", grow);
 b.addEventListener("click", clear);
 
 
